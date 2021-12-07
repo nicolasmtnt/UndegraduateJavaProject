373e2dcd5d4f3182f41662407a93c746f77af84a
Valentin Reault <valentin.reault@etu.univ-orleans.fr>
2021-12-07 10:53:11 +0100

    ajout code de databaseStock et modification de add dans Stock en consequence

--- src/main/java/Gestionnary/Stock.java
+++ src/main/java/Gestionnary/Stock.java
@@ -36,7 +36,13 @@ public class Stock{
     }
 
     public static void add(UUID uuid, int quantity){
+        File file = new File("src/main/ressources/databaseStock.csv");
+        BufferedWriter fo = new BufferedWriter(new FileWriter(file,true));
         try{
+            message = uuid.toString() + "," + quantity+"\n";
+            fo.write(message);
+            fo.close();
+
             map.put(uuid, map.get(uuid)+quantity);
         } catch (NullPointerException e){
             map.put(uuid, quantity);
